41b1b5e8443856f73f7d9a746895775abe9ea8de
Ana Popova <ana.popova83@gmail.com>
2024-01-18 13:14:47 +0200

    func(design): change data.source file

--- db/data.source.ts
+++ db/data.source.ts
@@ -1,11 +1,15 @@
 import { DataSource, DataSourceOptions } from "typeorm";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '123',
-  database: 'warehouse',
+  
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '123',
+  database: process.env.DB_DATABASE || 'warehouse',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
   synchronize: false
@@ -14,3 +18,4 @@ export const dataSourceOptions: DataSourceOptions = {
 const dataSource = new DataSource(dataSourceOptions);
 
 export default dataSource;
+
